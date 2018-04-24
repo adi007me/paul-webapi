552208d05a00761efff18ee71a1910be6c2e0838
Aditya Bhave <adi007me@gmail.com>
2018-04-23 23:50:07 -0400

    Bet Info Implementation

--- controllers/index.js
+++ controllers/index.js
@@ -1,14 +1,17 @@
 ﻿(function (controllers) {
     controllers.init = function (app) {
         var adminController = require('./adminController');
+        var betInfoController = require('./betInfoController');        
         var choicesController = require('./choicesController');
         var homeController = require('./homeController');
         var leaderBoardController = require('./leaderBoardController');
         var leaguesController = require('./leagueController');
         var teamsController = require('./teamsController');
         var userController = require('./userController');
+        
                 
         adminController.init(app);
+        betInfoController.init(app);
         choicesController.init(app);
         homeController.init(app);
         leaderBoardController.init(app);
