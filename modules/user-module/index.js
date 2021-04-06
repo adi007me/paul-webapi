0909e234c4be3029318ae17c4fc794711665f071
Aditya Bhave <adi007me@gmail.com>
2021-04-06 18:52:17 +0530

    Allow only globant users

--- modules/user-module/index.js
+++ modules/user-module/index.js
@@ -2,6 +2,10 @@
     const db = require('../../data');
 
     userModule.createOrGetUser = (user, next) => {
+        if (!e.endsWith('globant.com')) {
+            next('User not allowed', null)
+        }
+
         db.getUser(user.email, (err, dbUser) => {
             if (err) {
                 next(err);
